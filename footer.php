5ae760da9e84547bfe33a1d2a2c3471d61ca715d
Marlon Nino <ninomarlonvilla@gmail.com>
2019-08-10 15:16:13 +0800

    Update navbar active link
    Signed-off-by: Marlon Nino <ninomarlonvilla@gmail.com>

--- footer.php
+++ footer.php
@@ -59,6 +59,7 @@
     <script src="js/jquery.easing.min.js"></script>
     <script type="text/javascript" src="js/fliplightbox.min.js"></script>
     <script src="js/functions.js"></script>
+    <script src="js/custom.js"></script>
 
     <!-------------------------------------------------END-OF-BODY------------------------------------------------->
     </body>
---
+++ js/custom.js
@@ -0,0 +1,25 @@
+
+window.onload = function(){
+    /* menu link selector, home page, highlight class */
+    navHighlight('.navbar-nav li a', 'home.php', 'active'); 
+}
+
+function navHighlight(elem, home, active) {
+    let url = location.href.split("/");
+        currentLocation = url[url.length - 1];
+        link = document.querySelectorAll(elem);
+        // console.log(`inside nav: ${currentLocation} ${link}`);
+
+    for (let i = 0; i < link.length; i++) {
+        let path = link[i].href.split("/");
+            page = path[path.length - 1];
+
+        if (page == currentLocation || page == home && currentLocation == "") {
+            link[i].parentNode.className += " " + active;
+            document.body.className += ' ' + page.substr(0, page.lastIndexOf('.'));
+        }        
+    }
+
+}
+
+navHighlight('.menu a', 'home.php', 'active'); /* menu link selector, home page, highlight class */
